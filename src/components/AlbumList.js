caf4025234225726bafa98b3e0c6cf48c48469da
sethcarrino <sethcarrino@gmail.com>
2017-09-07 15:33:54 -0400

    created sections - applied styling with styled components - pulling data with API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!</Text>
-    </View>
-  );
-};
+// for api fetch requests
+import axios from 'axios';
+
+// importing Albim Detail
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  // lifecycle method - automatically get called when rendered
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
 
 export default AlbumList;
